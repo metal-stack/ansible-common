ff150bd6dffacd674af8be5bd225f1e973a4e9c5
Gerrit <tirreg91@googlemail.com>
2025-05-13 16:16:25 +0200

    Move.

--- filter_plugins/gardener.py
+++ filter_plugins/gardener.py
@@ -8,6 +8,8 @@ import json
 
 from kubernetes import config, client
 
+from ansible.plugins.test.core import version_compare
+
 
 def shoot_admin_kubeconfig(kubeconfig, project_namespace, shoot_name, expiry_seconds=28800):
     data = yaml.safe_load(kubeconfig)
@@ -36,8 +38,88 @@ def shoot_admin_kubeconfig(kubeconfig, project_namespace, shoot_name, expiry_sec
     return base64.b64decode(json.loads(response.data)["status"]["kubeconfig"]).decode('utf-8')
 
 
+def machine_images_for_cloud_profile(image_list, cris=None, compatibilities=None):
+    images = dict()
+    for image in image_list:
+        if 'machine' not in image.get("features", list()):
+            continue
+
+        if image.get('omit_from_cloud_profile', False):
+            continue
+
+        image_id = image.get("id")
+        if image_id is None:
+            continue
+
+        parts = image_id.split("-")
+        name = "-".join(parts[:-1])
+
+        version = parts[-1]
+
+        version_parts = version.split(".")
+        # ubuntu-19.10.20200331
+        # major = version_parts[0]
+        minor = ".".join(version_parts[:2])
+
+        image_versions = images.get(name, set())
+        # Do not add the major version to the vector
+        # metal-api cannot match latest version if only major is given
+        # image_versions.add(major)
+        image_versions.add(minor)
+        image_versions.add(version)
+        images[name] = image_versions
+
+    result = list()
+    for name, value in images.items():
+        versions = list()
+        for v in sorted(list(value)):
+            version = dict(
+                version=v
+            )
+
+            if cris is not None and name in cris:
+                cri = cris[name].copy()
+                cri_config = cri.pop("cris", [])
+                cri_condition = cri.pop("when", None)
+
+                if cri_condition is None:
+                    version["cri"] = cri_config
+                else:
+                    if v in cri_condition.get("except", []):
+                        pass
+                    else:
+                        if version_compare(v, cri_condition["version"], cri_condition["operator"]):
+                            version["cri"] = cri_config
+
+            if compatibilities is not None and name in compatibilities:
+                compat = compatibilities[name].copy()
+
+                kubelet = compat.pop("kubelet")
+                condition = compat.pop("when", None)
+
+                if condition is None:
+                    version["kubeletVersionConstraint"] = kubelet
+                else:
+                    if v in condition.get("except", []):
+                        pass
+                    else:
+                        if version_compare(v, condition["version"], condition["operator"]):
+                            version["kubeletVersionConstraint"] = kubelet
+
+            versions.append(version)
+
+        image = dict(
+            name=name,
+            versions=versions,
+        )
+        result.append(image)
+
+    return result
+
+
 class FilterModule(object):
     def filters(self):
         return {
             'shoot_admin_kubeconfig': shoot_admin_kubeconfig,
+            'machine_images_for_cloud_profile': machine_images_for_cloud_profile,
         }
--- test/filter_plugins_test.py
+++ test/filter_plugins_test.py
@@ -1,12 +1,15 @@
 import sys
 import unittest
 import json
+import yaml
 
 from test import FILTER_PLUGINS_PATH, read_mock_file
 
 sys.path.insert(0, FILTER_PLUGINS_PATH)
 import common
 import gcp
+import gardener
+
 
 class UtilsFilterTest(unittest.TestCase):
     def test_all_filters_present(self):
@@ -39,3 +42,236 @@ class GCPTest(unittest.TestCase):
         actual = gcp.extract_gcp_node_network(subnets, "europe-west3")
 
         self.assertEqual("0.0.0.3/32", actual)
+
+
+class CloudProfileImageTest(unittest.TestCase):
+    def test_image_list(self):
+        self.maxDiff = None
+
+        sample_data = """
+        - id: firewall-ubuntu-2.0.20200714
+          name: Firewall 2 Ubuntu 20200714
+          description: Firewall 2 Ubuntu 20200714
+          url: http://images.metal-stack.io/metal-os/master/firewall/2.0-ubuntu/20200714/img.tar.lz4
+          features:
+            - firewall
+            - machine
+        - id: firewall-2.0.20200714
+          name: Firewall 2 Debian 20200714
+          description: Firewall 2 Debian 20200714
+          url: http://images.metal-stack.io/metal-os/master/firewall/2.0/20200714/img.tar.lz4
+          features:
+            - firewall
+        - id: ubuntu-19.10.20200331
+          name: Ubuntu 19.10 20200331
+          description: Ubuntu 19.10 20200331
+          url: http://images.metal-stack.io/metal-os/ubuntu/19.10/20200331/img.tar.lz4
+          features:
+            - machine
+        - id: ubuntu-19.10.20200701
+          name: Ubuntu 19.10 20200701
+          description: Ubuntu 19.10 20200701
+          url: http://images.metal-stack.io/metal-os/ubuntu/19.10/20200701/img.tar.lz4
+          features:
+            - machine
+        - id: ubuntu-20.04.20200331
+          name: Ubuntu 20.04 20200331
+          description: Ubuntu 20.04 20200331
+          url: http://images.metal-stack.io/metal-os/ubuntu/20.04/20200331/img.tar.lz4
+          features:
+            - machine
+        - id: debian-10.0.20200331
+          name: Debian 10.0.20200331
+          description: Debian 10 20200331
+          url: http://images.metal-stack.io/metal-os/debian/10/20200331/img.tar.lz4
+          features:
+            - machine
+        - id: centos-7.0.20210620
+          name: Centos 7 20210620
+          description: Centos 7 20210620
+          url: http://images.metal-stack.io/metal-os/master/centos/7/20210620/img.tar.lz4
+          omit_from_cloud_profile: yes
+          features:
+              - machine
+        """
+        actual = gardener.machine_images_for_cloud_profile(yaml.safe_load(sample_data))
+        expected = [
+            {
+                "name": "firewall-ubuntu",
+                "versions": [
+                    {"version": "2.0"},
+                    {"version": "2.0.20200714"},
+                ]
+            },
+            {
+                "name": "ubuntu",
+                "versions": [
+                    {"version": "19.10"},
+                    {"version": "19.10.20200331"},
+                    {"version": "19.10.20200701"},
+                    {"version": "20.04"},
+                    {"version": "20.04.20200331"},
+                ],
+            },
+            {
+                "name": "debian",
+                "versions": [
+                    {"version": "10.0"},
+                    {"version": "10.0.20200331"},
+                ],
+            },
+        ]
+
+        self.assertListEqual(actual, expected)
+
+    def test_image_list_with_cri_mapping(self):
+        self.maxDiff = None
+
+        sample_data = """
+        - id: firewall-ubuntu-2.0.20200714
+          name: Firewall 2 Ubuntu 20200714
+          description: Firewall 2 Ubuntu 20200714
+          url: http://images.metal-stack.io/metal-os/master/firewall/2.0-ubuntu/20200714/img.tar.lz4
+          features:
+            - firewall
+            - machine
+        - id: firewall-2.0.20200714
+          name: Firewall 2 Debian 20200714
+          description: Firewall 2 Debian 20200714
+          url: http://images.metal-stack.io/metal-os/master/firewall/2.0/20200714/img.tar.lz4
+          features:
+            - firewall
+        - id: ubuntu-19.10.20200331
+          name: Ubuntu 19.10 20200331
+          description: Ubuntu 19.10 20200331
+          url: http://images.metal-stack.io/metal-os/ubuntu/19.10/20200331/img.tar.lz4
+          features:
+            - machine
+        - id: ubuntu-19.10.20200701
+          name: Ubuntu 19.10 20200701
+          description: Ubuntu 19.10 20200701
+          url: http://images.metal-stack.io/metal-os/ubuntu/19.10/20200701/img.tar.lz4
+          features:
+            - machine
+        - id: ubuntu-20.04.20200331
+          name: Ubuntu 20.04 20200331
+          description: Ubuntu 20.04 20200331
+          url: http://images.metal-stack.io/metal-os/ubuntu/20.04/20200331/img.tar.lz4
+          features:
+            - machine
+        """
+
+        cris = [
+            {
+                "name": "containerd",
+                "containerRuntimes": [
+                    {"type": "gvisor"},
+                    {"type": "kata-containers"},
+                ],
+            }
+        ]
+        cri_mapping = {
+            "ubuntu": {
+                "when": {
+                    "operator": ">=",
+                    "version": "20.04",
+                },
+                "cris": cris,
+            }
+        }
+
+        actual = gardener.machine_images_for_cloud_profile(yaml.safe_load(sample_data), cris=cri_mapping)
+        expected = [
+            {
+                "name": "firewall-ubuntu",
+                "versions": [
+                    {"version": "2.0"},
+                    {"version": "2.0.20200714"},
+                ]
+            },
+            {
+                "name": "ubuntu",
+                "versions": [
+                    {"version": "19.10"},
+                    {"version": "19.10.20200331"},
+                    {"version": "19.10.20200701"},
+                    {
+                        "version": "20.04",
+                        "cri": [
+                            {
+                                "name": "containerd",
+                                "containerRuntimes": [
+                                    {"type": "gvisor"},
+                                    {"type": "kata-containers"},
+                                ],
+                            }
+                        ],
+                    },
+                    {
+                        "version": "20.04.20200331",
+                        "cri": [
+                            {
+                                "name": "containerd",
+                                "containerRuntimes": [
+                                    {"type": "gvisor"},
+                                    {"type": "kata-containers"},
+                                ],
+                            }
+                        ],
+                    },
+                ],
+            },
+        ]
+
+        self.assertListEqual(actual, expected)
+
+
+    def test_image_list_with_compatibility_mapping(self):
+        self.maxDiff = None
+
+        sample_data = """
+        - id: debian-12.0.20240101
+          name: Debian 12 20240101
+          description: Debian 12 20240101
+          url: http://images.metal-stack.io/metal-os/debian/12/img.tar.lz4
+          features:
+            - machine
+        - id: debian-12.0.20250231
+          name: Debian 12 20250231
+          description: Debian 12 20250231
+          url: http://images.metal-stack.io/metal-os/debian/12/img.tar.lz4
+          features:
+            - machine
+        """
+
+        compat_mapping = {
+            "debian": {
+                "when": {
+                    "operator": "<",
+                    "version": "12.0.20250101",
+                    "except": ["12.0"],
+                },
+                "kubelet": "<= 1.30",
+            }
+        }
+
+        actual = gardener.machine_images_for_cloud_profile(yaml.safe_load(sample_data), compatibilities=compat_mapping)
+        expected = [
+            {
+                "name": "debian",
+                "versions": [
+                    {
+                        "version": "12.0",
+                    },
+                    {
+                        "version": "12.0.20240101",
+                        "kubeletVersionConstraint": "<= 1.30"
+                    },
+                    {
+                        "version": "12.0.20250231",
+                    },
+                ],
+            },
+        ]
+
+        self.assertListEqual(actual, expected)

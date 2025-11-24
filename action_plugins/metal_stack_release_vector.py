f03fd0efe89c57837f82131a3f381d110e9cae8c
Gerrit <tirreg91@googlemail.com>
2025-11-24 18:17:37 +0100

    Factor out into func.

--- action_plugins/metal_stack_release_vector.py
+++ action_plugins/metal_stack_release_vector.py
@@ -448,32 +448,7 @@ class OciLoader():
             raise ImportError(
                 "opencontainers must be installed in order to resolve metal-stack oci release vectors")
 
-        if self._cosign_key or self._cosign_identity or self._cosign_issuer:
-            try:
-                bin_path = process.get_bin_path(
-                    "cosign", required=True, opt_dirs=None)
-
-                if self._username and self._password:
-                    subprocess.run(args=[bin_path, "login", "--username",  self._username, "--password-stdin=true"],
-                                   input=self._password, check=True, capture_output=True)
-
-                if self._cosign_key:
-                    subprocess.run(args=[bin_path, "verify", "--key", "env://PUBKEY", self._url],
-                                   env=dict(PUBKEY=self._cosign_key), check=True, capture_output=True)
-                    display.display(
-                        "- %s was verified successfully by public key through cosign" % self._url, color=C.COLOR_OK)
-                elif self._cosign_identity or self._cosign_issuer:
-                    subprocess.run(args=[bin_path, "verify", "--certificate-oidc-issuer", self._cosign_issuer,
-                                         "--certificate-identity", self._cosign_identity, self._url],
-                                   check=True, capture_output=True)
-                    display.display(
-                        "- %s was verified successfully by oidc-issuer through cosign" % self._url, color=C.COLOR_OK)
-            except ValueError as e:
-                raise FileNotFoundError("cosign needs to be installed: %s" %
-                                        to_native(e.message)) from e
-            except subprocess.CalledProcessError as e:
-                raise RuntimeError("cosign verification returned with exit code %s: %s" % (
-                    e.returncode, to_native(e.stderr))) from e
+        self._cosign_validate()
 
         blob = self._download_blob()
 
@@ -483,6 +458,38 @@ class OciLoader():
             return self._extract_tar_gzip(blob, dest=self._dest, filter=self._dest_filter)
         else:
             return self._extract_tar_gzip_file(blob, member=self._member)
+
+    def _cosign_validate(self):
+        if not self._cosign_key and not (self._cosign_identity and self._cosign_issuer):
+            return
+
+        try:
+            bin_path = process.get_bin_path(
+                "cosign", required=True, opt_dirs=None)
+        except ValueError as e:
+            raise FileNotFoundError("cosign needs to be installed: %s" %
+                                    to_native(e.message)) from e
+
+        try:
+            if self._username and self._password:
+                subprocess.run(args=[bin_path, "login", "--username",  self._username, "--password-stdin=true"],
+                               input=self._password, check=True, capture_output=True)
+
+            if self._cosign_key:
+                subprocess.run(args=[bin_path, "verify", "--key", "env://PUBKEY", self._url],
+                               env=dict(PUBKEY=self._cosign_key), check=True, capture_output=True)
+                display.display(
+                    "- %s was verified successfully by public key through cosign" % self._url, color=C.COLOR_OK)
+            elif self._cosign_identity or self._cosign_issuer:
+                subprocess.run(args=[bin_path, "verify", "--certificate-oidc-issuer", self._cosign_issuer,
+                                     "--certificate-identity", self._cosign_identity, self._url],
+                               check=True, capture_output=True)
+                display.display(
+                    "- %s was verified successfully by oidc-issuer through cosign" % self._url, color=C.COLOR_OK)
+
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError("cosign verification returned with exit code %s: %s" % (
+                e.returncode, to_native(e.stderr))) from e
 
     def _download_blob(self):
         opts = [WithDefaultName(self._namespace)]
